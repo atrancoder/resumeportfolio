575a5388dd695d75e8eefd9fdaf54ac2b01b9531
Alex Tran <alextran@Alexs-Laptop.hsd1.ca.comcast.net>
2026-02-11 23:42:05 -0800

    Initial portfolio website

---
+++ src/main/java/com/alextran/portfolio/ResumeportfolioApplication.java
@@ -0,0 +1,12 @@
+package com.alextran.portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeportfolioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResumeportfolioApplication.class, args);
+    }
+}
---
+++ src/main/java/com/alextran/portfolio/controller/PageController.java
@@ -0,0 +1,31 @@
+package com.alextran.portfolio.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("name", "Alex Tran");
+        model.addAttribute("title", "Software Developer");
+        return "index";
+    }
+
+    @GetMapping("/about")
+    public String about() {
+        return "about";
+    }
+
+    @GetMapping("/projects")
+    public String projects() {
+        return "projects";
+    }
+
+    @GetMapping("/resume")
+    public String resume() {
+        return "resume";
+    }
+}
