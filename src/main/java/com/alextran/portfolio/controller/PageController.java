91531c8df32148fb431c65aadfd19fbb14d790a1
Alex Tran <alextran@Alexs-Laptop.hsd1.ca.comcast.net>
2026-02-13 22:45:45 -0800

    Redesigned site with modern navbar and multi-page layout

--- src/main/java/com/alextran/portfolio/controller/PageController.java
+++ src/main/java/com/alextran/portfolio/controller/PageController.java
@@ -7,10 +7,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class PageController {
 
-    @GetMapping("/")
-    public String home(Model model) {
+    @GetMapping("/") public String home(Model model) {
         model.addAttribute("name", "Alex Tran");
         model.addAttribute("title", "Computer Science");
         return "index";
+    }
+
+    @GetMapping("/about")
+    public String about() {
+        return "about";
+    }
+
+    @GetMapping("/projects")
+    public String projects() {
+        return "projects";
+    }
+
+    @GetMapping("/resume")
+    public String resume() {
+        return "resume";
     }
 }
